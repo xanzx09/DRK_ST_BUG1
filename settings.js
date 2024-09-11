33a482b6a6407c852804057ba248ccb71202231f
Dark1016 <roisandro86@gmail.com>
2024-09-10 23:17:18 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ global.ownernomer = "50931461936"
 global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
-global.location = "America, Haiti, Port-au-Prince"
+global.location = "America/Port-au-Prince"
 
 global.ownernumber = '50931461936'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
