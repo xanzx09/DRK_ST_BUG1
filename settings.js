b0940c956927f99f5649eba5a50c4cd0d5e595a8
xanzx09 <zarinawahab171798@gmail.com>
2025-02-08 16:00:28 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
+global.ownernomer = "60175263177"
 global.ownername = "🌹⃟⃢👑 ༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America/Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
+global.ownernumber = '60175263177'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
